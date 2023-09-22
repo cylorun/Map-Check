35f0046a293351dcaaf010a659a6b3f93976d026
cylorun <109686521+cylorun@users.noreply.github.com>
2023-09-22 13:10:19 +0100

    clea

--- src/main/java/me/cylorun/Panel.java
+++ src/main/java/me/cylorun/Panel.java
@@ -98,7 +98,46 @@ public class Panel extends JPanel {
         selectInstances.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                new InstanceSelect();
+                JFileChooser fileChooser = new JFileChooser();
+                String defaultDir = System.getProperty("user.home") + "\\Desktop";
+
+                fileChooser.setCurrentDirectory(new File(defaultDir));
+                fileChooser.setMultiSelectionEnabled(true);
+                fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                int response = fileChooser.showOpenDialog(null);
+
+                if (response == JFileChooser.APPROVE_OPTION) {
+                    File[] selectedFiles = fileChooser.getSelectedFiles();
+
+                    for (File file : selectedFiles) {
+                        File savesFolder = new File(file + "\\.minecraft\\saves");
+
+                        if (savesFolder.exists()) {
+                            FileUtil.instancePaths.add(savesFolder.getAbsolutePath());
+                        } else {
+
+                            int choice = JOptionPane.showConfirmDialog(null, file.getAbsolutePath() + "\n is not a minecraft directory \n Would you still like to add it?", "Invalid Directory", JOptionPane.YES_NO_OPTION);
+
+                            if (choice == JOptionPane.YES_OPTION) {
+                                boolean matchFound = false;
+
+                                for (File f : file.listFiles()) {
+                                    if (f.getName().contains(".minecraft")) {
+                                        savesFolder.mkdir();
+                                        FileUtil.instancePaths.add(savesFolder.getAbsolutePath());
+                                        matchFound = true;
+                                        break;
+                                    }
+                                }
+                                if (!matchFound) {
+                                    FileUtil.instancePaths.add(file.getAbsolutePath());
+                                }
+
+                            }
+                        }
+                    }
+                }
+
             }
         });
 
